40a1bc5b320d973f26938ccf6b88fe1ab1cea6fc
retransfy <142636606+retransfy@users.noreply.github.com>
2023-09-22 18:03:48 +0000

    Update display.js

--- display.js
+++ display.js
@@ -6,21 +6,7 @@ let transactionDetails = decodeURIComponent(encodedMessage);
 document.getElementById("messageOutput").innerHTML = transactionDetails;
 let copyButton = document.getElementById("copyButton");
 
-/*copyButton.addEventListener("click", function () {
-  const textToCopy = transactionDetails.replace(/<[^>]*>/g, '');
-  copyToClipboard(textToCopy);
-  copyButton.innerHTML = "Transaction Details Copied";
-  copyButton.style.backgroundColor = "gray";
-  copyButton.disabled = true;
-  alert(`You've been restricted to use the payment function. Send the transaction details to Our official whatsApp page to complete your transaction. Click "OK" to copy the transaction details`)
-});
-
-
-function copyToClipboard(text) {
-  navigator.clipboard.writeText(text)
-}*/
-
-    copyButton.addEventListener("click", function () {
+copyButton.addEventListener("click", function () {
       const textToCopy = transactionDetails.replace(/<[^>]*>/g, '');
       copyToClipboard(textToCopy);
       copyButton.innerHTML = "Transaction Details Copied";
